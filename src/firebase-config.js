3d6ecbe1e824a1b2045e055f91003db3f77b37c3
harish khope <harishkhope89@gmail.com>
2023-07-22 22:43:57 +0530

    removed config details

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import {getFirestore} from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCf2reXwh4yJyuEsoTFqfUaqSfeG46PgMM",
-  authDomain: "library-crud-4dd3c.firebaseapp.com",
-  projectId: "library-crud-4dd3c",
-  storageBucket: "library-crud-4dd3c.appspot.com",
-  messagingSenderId: "148408378823",
-  appId: "1:148408378823:web:f1224e5eb2e3023cac25e2",
-  measurementId: "G-15SZ6EPYYV"
+  apiKey: "your api key",
+  authDomain: "your auth domain",
+  projectId: "your project id",
+  storageBucket: "your storage bucket",
+  messagingSenderId: "your messaging sender id",
+  appId: "your app id",
+  measurementId: "your measurement id",
 };
 
 // Initialize Firebase
