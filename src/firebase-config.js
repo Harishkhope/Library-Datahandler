dcae78f8b3ded2d36ea1768e0cd0afcaeac67789
harish khope <harishkhope89@gmail.com>
2023-07-24 17:46:28 +0530

    restored config files

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -6,7 +6,7 @@ const firebaseConfig = {
   apiKey: "your api key",
   authDomain: "your auth domain",
   projectId: "your project id",
-  storageBucket: "storage bucket",
+  storageBucket: "your storage bucket",
   messagingSenderId: "your messaging sender id",
   appId: "your app id",
   measurementId: "your measurement id",
