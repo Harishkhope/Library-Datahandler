f5d6799fff7e4eb9798dba44cd0881bb14ce3df0
harish khope <harishkhope89@gmail.com>
2023-07-24 17:24:57 +0530

    changed config file

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -6,7 +6,7 @@ const firebaseConfig = {
   apiKey: "your api key",
   authDomain: "your auth domain",
   projectId: "your project id",
-  storageBucket: "your storage bucket",
+  storageBucket: "storage bucket",
   messagingSenderId: "your messaging sender id",
   appId: "your app id",
   measurementId: "your measurement id",
